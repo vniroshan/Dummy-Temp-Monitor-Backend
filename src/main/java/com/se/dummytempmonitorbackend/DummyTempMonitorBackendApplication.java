8cfe15f07b36402c686cd51c67e7a265114989f7
Tharun <tharunvar10@gmail.com>
2020-12-14 16:01:12 +0530

    Initial Project

---
+++ src/main/java/com/se/dummytempmonitorbackend/DummyTempMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.se.dummytempmonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyTempMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyTempMonitorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/dummytempmonitorbackend/DummyTempMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.dummytempmonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyTempMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
